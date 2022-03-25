119e33398636c39f5d11fd9bc7441e716f6380a0
guhuan <2371906989@qq.com>
2022-03-25 09:37:42 +0800

    lambda 四大函数接口

--- src/test/java/tett.java
+++ src/test/java/tett.java
@@ -1,11 +1,44 @@
 import com.gu.interfaces.Test01;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.*;
+
 public class tett {
     @Test
     public void test(){
-        Test01 test01 = System.out::println;
+        /*
+        * Consumer 消费型
+        * parameter T
+        * return null
+        * */
+        Consumer<String> consumer = System.out::println;
+        consumer.accept("hello word");
+        
+        /*
+        * Supplier 生产型
+        * parameter null
+        * return T
+        * */
+        Date date = new Date();
+        Supplier<Long> stringSupplier = date::getTime;
+        System.out.println("stringSupplier.get() = " + stringSupplier.get());
+        /*
+         * Function
+         * parameter
+         * return
+         * */
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Function<Date,String> function = format::format;
+        System.out.println(function.apply(new Date()));
 
-        test01.test("hello word");
+        /*
+         * Function
+         * parameter <T, U>
+         * return bool
+         * */
+        BiPredicate<String,String> predicate = String::equals;
+        System.out.println(predicate.test("aa","aa"));
     }
 }
