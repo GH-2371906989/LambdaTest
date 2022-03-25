9e4554e2917fba40d596b3c240e395b7a777199a
guhuan <2371906989@qq.com>
2022-03-25 09:38:40 +0800

    lambda 四大函数接口

--- src/test/java/FiveFunction.java
+++ src/test/java/FiveFunction.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.function.*;
 
-public class tett {
+public class FiveFunction {
     @Test
     public void test(){
         /*
